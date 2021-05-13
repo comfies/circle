681caf6a3cb4fe0b76987c9d52187a1ef1dfce10
alice <notminin@pm.me>
2021-05-13 19:24:18 +0100

    comvert to tabs :D

--- builder/__main__.py
+++ builder/__main__.py
@@ -16,51 +16,51 @@ dest = args.destination
 
 
 with open(source, encoding="utf8") as f:
-    sites = tomlkit.parse(f.read())["sites"]
+	sites = tomlkit.parse(f.read())["sites"]
 
 
 if dest.exists():
-    raise FileExistsError("Destination already exists")
+	raise FileExistsError("Destination already exists")
 dest.mkdir()
 dest.joinpath("f").mkdir()
 
 
 for n, site in enumerate(sites):
-    prev = sites[(n - 1) % len(sites)]
-    next = sites[(n + 1) % len(sites)]
-    dest.joinpath("f", site["id"]).mkdir()
-    with open(dest.joinpath("f", site["id"], "index.html"), "x", encoding="utf8") as f:
-        f.write(
-            dedent(
-                f"""\
-                <!doctype html>
-                <title>Circle - {html.escape(site['id'])}</title>
-                <a id=all href=/ target=_parent>comfi.es Circle</a>
-                <a id=prev href="{html.escape(prev['url'])}" target=_parent>Prev</a>
-                <a id=next href="{html.escape(next['url'])}" target=_parent>Next</a>
-                """
-            )
-        )
+	prev = sites[(n - 1) % len(sites)]
+	next = sites[(n + 1) % len(sites)]
+	dest.joinpath("f", site["id"]).mkdir()
+	with open(dest.joinpath("f", site["id"], "index.html"), "x", encoding="utf8") as f:
+		f.write(
+			dedent(
+				f"""\
+				<!doctype html>
+				<title>Circle - {html.escape(site['id'])}</title>
+				<a id=all href=/ target=_parent>comfi.es Circle</a>
+				<a id=prev href="{html.escape(prev['url'])}" target=_parent>Prev</a>
+				<a id=next href="{html.escape(next['url'])}" target=_parent>Next</a>
+				"""
+			)
+		)
 
 with open(dest.joinpath("404.html"), "x", encoding="utf8") as f:
-    f.write(
-        dedent(
-            f"""\
-            <!doctype html>
-            <title>Circle - {html.escape(site['id'])}</title>
-            <h1><a id=all href=/ target=_parent>comfi.es Circle</a></h1>
-            <p><code>404</code> Page not found.
-            """
-        )
-    )
+	f.write(
+		dedent(
+			f"""\
+			<!doctype html>
+			<title>Circle - {html.escape(site['id'])}</title>
+			<h1><a id=all href=/ target=_parent>comfi.es Circle</a></h1>
+			<p><code>404</code> Page not found.
+			"""
+		)
+	)
 
 with open(dest.joinpath("f", "404.html"), "x", encoding="utf8") as f:
-    f.write(
-        dedent(
-            f"""\
-            <!doctype html>
-            <title>Circle - {html.escape(site['id'])}</title>
-            <a id=all href=/ target=_parent>comfi.es Circle</a>
-            """
-        )
-    )
+	f.write(
+		dedent(
+			f"""\
+			<!doctype html>
+			<title>Circle - {html.escape(site['id'])}</title>
+			<a id=all href=/ target=_parent>comfi.es Circle</a>
+			"""
+		)
+	)
